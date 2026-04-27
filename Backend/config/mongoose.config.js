113cc2fc4a52e1780af25df00d4462e308a9fb54
ayan <24ucs219@lnmiit.ac.in>
2026-04-27 15:50:39 +0530

    update env for atlas

--- Backend/config/mongoose.config.js
+++ Backend/config/mongoose.config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async ()=>{
     
     try{
-        const connectionInstance= await mongoose.connect('mongodb://127.0.0.1:27017/solarCurtailmentOptimizer');
+        const connectionInstance= await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected!!');
         
     }catch(err){
