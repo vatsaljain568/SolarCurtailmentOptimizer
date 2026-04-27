7993ed9d10e1e211bcc5c3a774a611b5031854ba
ayan <24ucs219@lnmiit.ac.in>
2026-04-27 17:53:30 +0530

    debugging2

--- Backend/hash.js
+++ Backend/hash.js
@@ -1,24 +1,26 @@
+// seed.js
+const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const User = require('./models/operator.model')
-const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
-
-function main(){
-    bcrypt.genSalt(10, (err,salt)=>{
-        password = 'password#123';
-        bcrypt.hash(password, salt, async(err,hash)=>{
-            let createdUser = await User.create({
-                password: hash ,
-                name: 'Dev Shukla',
-                email: 'devshukla@gmail.com'
-            })
-
-            console.log(hash);
-            
-            
-            
-        })
+async function main() {
+    await mongoose.connect(process.env.MONGO_URI)
+    
+    // delete old user first
+    await User.deleteMany({ email: 'devshukla@gmail.com' })
+    
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash('password#123', salt)
+    
+    await User.create({
+        name: 'Dev Shukla 2',
+        email: 'devshukla@gmail.com',
+        password: hash
     })
+    
+    console.log('User created successfully')
+    mongoose.disconnect()
 }
 
-module.exports = {main}
+main()
